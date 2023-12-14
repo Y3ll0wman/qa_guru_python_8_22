745dd6de3128bcf8d2fbcc54abf6d360fefe13a7
Y3ll0w <k.varbarkin@gmail.com>
2023-12-14 21:55:52 +0300

    Fix remote url

---
+++ tests/conftest.py
@@ -0,0 +1,49 @@
+import allure
+import allure_commons
+import pytest
+# import config
+
+from appium.options.android import UiAutomator2Options
+from selene import browser, support
+from appium import webdriver
+# from qa_guru_python_8_22.utils import allure_attach
+
+
+@pytest.fixture(scope='function')
+def android_mobile_management():
+    options = UiAutomator2Options().load_capabilities({
+        # 'deviceName': 'Pixel_3a_API_34_extension_level_7_x86_64',
+        # 'deviceName': 'sdk_gphone64_x86_64',
+        "platformName": "android",
+        'deviceName': 'emulator-5554',
+        'appWaitActivity': 'org.wikipedia.*',
+        'app': 'C:\\Users\\Y3ll0w\\Desktop\\app-alpha-universal-release.apk',
+    })
+
+    # browser.config.driver_remote_url = remote_browser_url
+    # browser.config.driver_options = options
+
+    with allure.step('setup app session'):
+        browser.config.driver = webdriver.Remote(
+            'http://127.0.0.1:4723',
+            options=options
+        )
+
+    browser.config.timeout = 10.0
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext
+    )
+
+    yield
+
+    # allure_attach.screenshot()
+
+    # allure_attach.page_source_xml()
+
+    # session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    # allure_attach.bstack_video(session_id)
