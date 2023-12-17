8e7e79933d3f6f3ea7be12a8939d0219e2c38398
Y3ll0w <k.varbarkin@gmail.com>
2023-12-17 15:47:29 +0300

    Fix all tests

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -5,32 +5,34 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
-def test_search(android_mobile_management):
+def test_search():
     # WHEN
     with step('Press Skip button'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
 
     with step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+        # browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_container')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Android')
 
     # THEN
     with step('Verify content found'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
-        results.first.should(have.text('Appium'))
+        results.first.should(have.text('Android'))
 
 
-def test_open_article(android_mobile_management):
+def test_open_article():
     # WHEN
     with step('Press Skip button'):
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
 
     with step('Search article'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+        # browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_container')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('Android')
         browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).click()
 
     # THEN
     with step('Verify article title'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Appium'))
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Android'))
